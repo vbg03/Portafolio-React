89742abaa323e84c03a0d4ce9f188ae12d5bab04
vbg03 <valeria.bowers@uao.edu.co>
2024-09-29 21:41:09 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./assets/Componentes/Header.jsx";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Aquí puedes agregar más componentes como Habilidades, Proyectos, Footer, etc. */}
+    </div>
+  );
+}
+
+export default App;
