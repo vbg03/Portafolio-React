a831bd5340c55b2649e2002c9620ef5bc77e33b6
vbg03 <valeria.bowers@uao.edu.co>
2024-10-02 23:04:46 -0500

    Componente Habilidades terminado

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Footer from './assets/Componentes/footer/Footer.jsx';
 import Inicio from './assets/Componentes/inicio/Inicio.jsx';
 import SobreMi from './assets/Componentes/sobre-mi/SobreMi.jsx';
+import Habilidades from './assets/Componentes/habilidades/Habilidades.jsx';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Inicio />
       <SobreMi />
+      <Habilidades />
       <Footer />
       {/* Aquí puedes agregar más componentes como Habilidades, Proyectos, Footer, etc. */}
     </div>
---
+++ src/assets/Componentes/habilidades/Habilidades.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Habilidades = () => {
+    return (
+        <section class="habilidades" id='habilidades'>
+            <h2 class="heading">Mis <span>Habilidades</span></h2>
+
+            <div class="habilidades-container">
+                <div class="habilidades-box">
+                    <i class='bx bx-code-block'></i>
+                    <h3>Habilidades Duras</h3>
+                    <p>hola</p>
+                </div>
+
+                <div class="habilidades-box">
+                    <i class='bx bx-message-dots' ></i>
+                    <h3>Habilidades Blandas</h3>
+                    <p>hola</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Habilidades
