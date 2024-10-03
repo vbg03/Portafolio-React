6575e11b6eca615b333a1dcaceeda8adf6975e66
vbg03 <valeria.bowers@uao.edu.co>
2024-10-02 21:28:54 -0500

    Componente SobreMi terminado

--- src/assets/Componentes/sobre-mi/SobreMi.jsx
+++ src/assets/Componentes/sobre-mi/SobreMi.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const SobreMi = () => {
   return (
-    <section>
-        
+    <section class="sobre" id='sobre'>
+        <div class="sobre-img">
+            <img src="./public/Imagenes/yo.png" alt="" />
+        </div>
+
+        <div class="sobre-content">
+            <h2 class="heading">Sobre <span>Mi</span></h2>
+            <h3>Ingeniera Multimedia</h3>
+            <p>hola</p>
+            <a href="#" class="btn">Leer Más</a>
+        </div>
     </section>
   )
 }
