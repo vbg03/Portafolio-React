1504a1e6d591d62cab5ae6b4d25be2940ca0f94e
vbg03 <valeria.bowers@uao.edu.co>
2024-10-02 23:44:53 -0500

    Componente footer terminado

--- src/assets/Componentes/footer/Footer.jsx
+++ src/assets/Componentes/footer/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-        <p>&copy; 2024 Valeria Bowers Gutierrez. Todos los derechos reservados.</p>
+    <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2024 por Valeria Bowers Gutierrez | Todos los derechos reservados.</p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#inicio"><i class='bx bxs-up-arrow-square' ></i></a>
+        </div>
     </footer>
 
   )
