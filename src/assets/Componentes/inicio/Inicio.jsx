7158f5675e3368c7d2ecccbfcac5e9549d693006
vbg03 <valeria.bowers@uao.edu.co>
2025-07-28 23:36:46 -0500

    terminando re-diseño

--- src/assets/Componentes/inicio/Inicio.jsx
+++ src/assets/Componentes/inicio/Inicio.jsx
@@ -14,7 +14,7 @@ const Inicio = () => {
                     <a href="https://www.instagram.com/itzzval__/" target="_blank" rel="noopener noreferrer"><i className='bx bxl-instagram-alt' ></i></a>
                     <a href="https://www.facebook.com/valeria.bowers.7/?locale=es_LA" target="_blank" rel="noopener noreferrer"><i className='bx bxl-facebook-square' ></i></a>
                     <a href="https://github.com/vbg03" target="_blank" rel="noopener noreferrer"><i className='bx bxl-github' ></i></a>
-                    <a href="https://www.linkedin.com/in/valeria-bowers-5b7ba9324/" target="_blank" rel="noopener noreferrer"><i className='bx bxl-linkedin-square' ></i></a>
+                    <a href="https://www.linkedin.com/in/valeria-bowers-gutierrez-a9a77a334/" target="_blank" rel="noopener noreferrer"><i className='bx bxl-linkedin-square' ></i></a>
                 </div>
             </div>
 
--- src/assets/Componentes/proyectos/Proyectos.jsx
+++ src/assets/Componentes/proyectos/Proyectos.jsx
@@ -7,61 +7,109 @@ const Proyectos = () => {
 
             <div className="proyectos-container">
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 1" />
                     <div className="proyectos-layer">
                         <h4>Evitar archivos binarios</h4>
-                        <a href="https://github.com/vbg03/web_clase2" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/vbg03/web_clase2" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
 
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 2" />
                     <div className="proyectos-layer">
                         <h4>Página responsive</h4>
-                        <a href="https://github.com/vbg03/Clase-3-web" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/vbg03/Clase-3-web" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
 
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 3" />
                     <div className="proyectos-layer">
                         <h4>Página de login</h4>
-                        <a href="https://github.com/edwinmgallego/clase-practica-css-jueves/tree/main/vbowers2215684" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/edwinmgallego/clase-practica-css-jueves/tree/main/vbowers2215684" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
 
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 4" />
                     <div className="proyectos-layer">
                         <h4>Número menor</h4>
-                        <a href="https://github.com/vbg03/Clase-8" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/vbg03/Clase-8" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
 
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 5" />
                     <div className="proyectos-layer">
                         <h4>Contador</h4>
-                        <a href="https://github.com/vbg03/Mi-primer-proyecto-react/tree/main" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/vbg03/Mi-primer-proyecto-react/tree/main" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
 
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 6" />
                     <div className="proyectos-layer">
                         <h4>Clase virtual</h4>
-                        <a href="https://github.com/vbg03/Clase-7" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/vbg03/Clase-7" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
 
                 <div className="proyectos-box">
-                    <img src="/Imagenes/github.png" alt="" />
+                    <img src="/Imagenes/github.png" alt="Proyecto 7" />
                     <div className="proyectos-layer">
                         <h4>Reproducir un video en react</h4>
-                        <a href="https://github.com/vbg03/Clase-10/tree/main" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external' ></i></a>
+                        <a 
+                            href="https://github.com/vbg03/Clase-10/tree/main" 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <i className='bx bx-link-external'></i>
+                        </a>
                     </div>
                 </div>
-
             </div>
         </section>
     )
