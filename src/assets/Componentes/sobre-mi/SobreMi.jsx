c68da2d87915678eeeb8737760f86403092d5ad3
vbg03 <valeria.bowers@uao.edu.co>
2025-08-01 21:30:44 -0500

    últimos arreglos

--- src/assets/Componentes/sobre-mi/SobreMi.jsx
+++ src/assets/Componentes/sobre-mi/SobreMi.jsx
@@ -13,7 +13,7 @@ const SobreMi = () => {
         <p>Soy estudiante de séptimo semestre de Ingeniería Multimedia en la Universidad Autónoma de Occidente.
           Apasionada por la tecnología y el diseño, estoy en constante aprendizaje para integrar la creatividad y la ingeniería en soluciones innovadoras.
           A lo largo de mi formación, he desarrollado habilidades en programación, diseño y trabajo en equipo, destacando mi paciencia y capacidad para gestionar el estrés en proyectos exigentes.</p>
-        <a href="#" className="btn">Leer Más</a>
+        {/*<a href="#" className="btn">Ver CV</a>*/}
       </div>
     </section>
   )
